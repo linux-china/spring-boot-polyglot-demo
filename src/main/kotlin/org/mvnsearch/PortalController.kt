86f60a03aebf1e4442124437675cd90d5244f405
linux_china <linux_china@hotmail.com>
2016-11-25 15:44:21 +0800

    portal controller

---
+++ src/main/kotlin/org/mvnsearch/PortalController.kt
@@ -0,0 +1,19 @@
+package org.mvnsearch
+
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+/**
+ * portal controller
+ *
+ * @author linux_china
+ */
+
+@RestController
+class CustomerController {
+    @RequestMapping("/")
+
+    fun index(): String {
+        return "good"
+    }
+}
