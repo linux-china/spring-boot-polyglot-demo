aac33108a3fe913339e1463a794bd29aad0d8954
linux_china <linux_china@hotmail.com>
2017-02-23 17:20:55 +0800

    add createdAt

--- src/main/kotlin/org/mvnsearch/PurchaseOrder.kt
+++ src/main/kotlin/org/mvnsearch/PurchaseOrder.kt
@@ -1,13 +1,19 @@
 package org.mvnsearch
 
+import java.util.*
+
 /**
  * purchase order
  *
  * @author linux_china
  */
 class PurchaseOrder(var buyerId: Long, var total: Double) {
-
+    /**
+     * created timestamp
+     */
+    val createdAt = Date()
+    
     operator fun plus(order2: PurchaseOrder): Double {
-       return this.total + order2.total
+        return this.total + order2.total
     }
 }
