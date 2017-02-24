6b83428e3ab01ef4e39eaee44aca8b9e5c72c8ba
linux_china <linux_china@hotmail.com>
2017-02-24 16:03:21 +0800

    user service implementation

---
+++ src/main/java/org/mvnsearch/UserServiceImpl.java
@@ -0,0 +1,16 @@
+package org.mvnsearch;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * user service implementation
+ *
+ * @author linux_china
+ */
+@Service
+public class UserServiceImpl implements UserService {
+
+    public void placeOrder(PurchaseOrder order) {
+
+    }
+}
