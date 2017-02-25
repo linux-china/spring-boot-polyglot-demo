0e4003d99d0000527cd19a2d6751aa3bbc83938c
linux_china <linux_china@hotmail.com>
2017-02-25 09:46:13 +0800

    Demo Application context

---
+++ src/main/java/org/mvnsearch/domain/DemoAppContext.java
@@ -0,0 +1,24 @@
+package org.mvnsearch.domain;
+
+import org.jetbrains.annotations.NotNull;
+import org.mvnsearch.domain.model.UserRepository;
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+
+/**
+ * Demo Application context
+ *
+ * @author linux_china
+ */
+@SuppressWarnings("NullableProblems")
+public class DemoAppContext implements ApplicationContextAware {
+
+    @NotNull
+    public static UserRepository userRepository;
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        userRepository = applicationContext.getBean(UserRepository.class);
+    }
+}
