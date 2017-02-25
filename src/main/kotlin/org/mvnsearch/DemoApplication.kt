8e1553990825187e2cec18c5f46d7d78ec2635ae
linux_china <linux_china@hotmail.com>
2017-02-25 09:47:16 +0800

    add Int.user()

--- src/main/kotlin/org/mvnsearch/DemoApplication.kt
+++ src/main/kotlin/org/mvnsearch/DemoApplication.kt
@@ -1,5 +1,7 @@
 package org.mvnsearch
 
+import org.mvnsearch.domain.DemoAppContext
+import org.mvnsearch.domain.model.User
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 
@@ -14,6 +16,13 @@ var running: Boolean = true
 @SpringBootApplication
 open class DemoApplication {
 
+}
+
+/**
+ * find user by ID
+ */
+fun Int.user(): User? {
+    return DemoAppContext.userRepository.findOne(this);
 }
 
 fun main(args: Array<String>) {
