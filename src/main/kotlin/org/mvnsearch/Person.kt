c73d9fd55f99bcbfc11ce42575ea5275f1843bb0
linux_china <linux_china@hotmail.com>
2017-07-26 18:22:56 +0800

    add repository

--- src/main/kotlin/org/mvnsearch/Person.kt
+++ src/main/kotlin/org/mvnsearch/Person.kt
@@ -1,11 +1,42 @@
 package org.mvnsearch
 
+import org.springframework.stereotype.Component
+
 /**
  * person
  *
  * @author linux_china
  */
-class Person{
-    var id: Int? =null
-    var nick: String? =null
+
+class Person {
+    var id: Int? = null
+    var nick: String? = null
+}
+
+interface PersonRepository {
+    @Deprecated("Please use findOne", ReplaceWith("findOne(id)"))
+    fun findById(id: Int): Person
+
+    fun findOne(id: Int): Person
+
+    fun findNick(id: Int): String
+}
+
+@Component
+open class PersonRepositoryImpl : PersonRepository {
+
+    override fun findById(id: Int): Person {
+        TODO("not implemented")
+    }
+
+    override fun findOne(id: Int): Person {
+        return Person().apply {
+            this.id = id
+            this.nick = "nick:" + id
+        }
+    }
+
+    override fun findNick(id: Int): String {
+        TODO("not implemented")
+    }
 }
