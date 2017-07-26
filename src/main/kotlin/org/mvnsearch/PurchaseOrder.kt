fc476a8468dc23a7ad75bf736e90de9fe5a74930
linux_china <linux_china@hotmail.com>
2017-07-26 18:22:05 +0800

    add id

--- src/main/kotlin/org/mvnsearch/PurchaseOrder.kt
+++ src/main/kotlin/org/mvnsearch/PurchaseOrder.kt
@@ -7,12 +7,12 @@ import java.util.*
  *
  * @author linux_china
  */
-class PurchaseOrder(var buyerId: Long, var total: Double) {
+class PurchaseOrder(var buyerId: Long, var total: Double, val id: String = Random().toString()) {
     /**
      * created timestamp
      */
     val createdAt = Date()
-    
+
     operator fun plus(order2: PurchaseOrder): Double {
         return this.total + order2.total
     }
