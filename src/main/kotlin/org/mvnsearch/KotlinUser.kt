e02e2a82a1fe9f034503685ace062ce5271c70dc
linux_china <libing.chen@gmail.com>
2019-05-17 10:40:34 -0700

    rename user to KotlinUser

---
+++ src/main/kotlin/org/mvnsearch/KotlinUser.kt
@@ -0,0 +1,9 @@
+package org.mvnsearch
+
+/**
+ * Kotlin user data class
+ *
+ * @author linux_china
+ */
+
+data class KotlinUser(val id: Int, val nick: String = "")
--- src/main/kotlin/org/mvnsearch/User.kt
+++
@@ -1,9 +0,0 @@
-package org.mvnsearch
-
-/**
- * user data class
- *
- * @author linux_china
- */
-
-data class User(val id: Int, val nick: String = "")
