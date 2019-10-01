de26da136934c713c73ff67f098cb3d9a14b2fbf
linux_china <libing.chen@gmail.com>
2019-09-30 19:55:24 -0700

    remove @RunWith(SpringRunner::class)

--- src/test/kotlin/org/mvnsearch/ApplicationTests.kt
+++ src/test/kotlin/org/mvnsearch/ApplicationTests.kt
@@ -7,11 +7,8 @@ package org.mvnsearch
  */
 
 import org.junit.Test
-import org.junit.runner.RunWith
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.test.context.junit4.SpringRunner
 
-@RunWith(SpringRunner::class)
 @SpringBootTest
 class ApplicationTests {
 
